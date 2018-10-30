23724f9bbfbee228cfcb658d459b1b4657a5be9a
Daniel Casasola <casasolad86@gmail.com>
2018-10-30 15:12:38 -0500

    created orders schema

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,11 +1,14 @@
 const User = require('./user')
 const Product = require('./product')
+const Order = require('./orders')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
  *
  *    BlogPost.belongsTo(User)
  */
+
+ //Associations
 
 /**
  * We'll export all of our models here, so that any time a module needs a model,
@@ -15,5 +18,6 @@ const Product = require('./product')
  */
 module.exports = {
   User,
-  Product
+  Product,
+  Order
 }
