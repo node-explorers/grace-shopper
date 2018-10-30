6e2469ea242b216f24ba6dd9688fdd5685b1f5f5
Mark Bassett <bassmarkie@gmail.com>
2018-10-30 16:20:37 -0500

    allproducts displays on main page

--- client/components/AllProduct.js
+++ client/components/AllProduct.js
@@ -1,49 +1,62 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {fetchProducts} from '../store/product'
-import {Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button} from 'reactstrap'
+import { connect } from 'react-redux'
+import { fetchProducts } from '../store/product'
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from 'reactstrap'
 
-function mapState(state){
-  return{
-    products:state.products
+function mapState(state) {
+  return {
+    products: state.products
   }
 }
-  function mapDispatch(dispatch){
-  return{
-    fetchingProduct:()=>{dispatch(fetchProducts())}
+function mapDispatch(dispatch) {
+  return {
+    fetchingProduct: () => {
+      dispatch(fetchProducts())
+    }
   }
 }
 
-
-export class AllProduct extends React.Component{
-  constructor(){
+export class AllProduct extends React.Component {
+  constructor() {
     super()
-
   }
-  componentDidMount(){
+  componentDidMount() {
     this.props.fetchingProduct()
   }
-  render(){
+  render() {
+    console.log(this.props)
+    if (!this.props.products) return <div>No Products</div>
+
     return (
       <div>
-        {this.props.products.map((product)=><div key={product.id}>
-        <Card>
-        <CardImg top width="100%" src={product.imageUrl} alt="Card image cap" />
-        <CardBody>
-          <CardTitle>{product.name}</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>{product.description}</CardText>
-          <Button color='primary' >Add to Cart</Button>
-        </CardBody>
-      </Card>
-
-        </div>)}
-
+        {this.props.products.map(product => (
+          <div key={product.id}>
+            <Card>
+              <CardImg
+                top
+                width="100%"
+                src={product.imageUrl}
+                alt="Card image cap"
+              />
+              <CardBody>
+                <CardTitle>{product.name}</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>{product.description}</CardText>
+                <Button color="primary">Add to Cart</Button>
+              </CardBody>
+            </Card>
+          </div>
+        ))}
       </div>
     )
   }
-
 }
-export default connect(mapState,
-  mapDispatch)(AllProduct)
+export default connect(mapState, mapDispatch)(AllProduct)
--- client/store/index.js
+++ client/store/index.js
@@ -1,16 +1,16 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import user from './user'
-import product from './product'
+import products from './product'
 
 const reducer = combineReducers({
   user,
-  product
+  products
 })
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 )
 const store = createStore(reducer, middleware)
 
