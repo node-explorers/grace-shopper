c54fbb52a31e13b13ef846edad2c4d4a922dea0d
Mark Bassett <bassmarkie@gmail.com>
2018-10-31 16:39:56 -0500

    made card display function

---
+++ client/components/MakeCard.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
+const styles = {
+  card: {
+    maxWidth: 345
+  },
+  media: {
+    height: 140
+  }
+}
+
+function MakeCard(props) {
+  const { classes, product } = props
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={product.imageUrl}
+          title={product.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {product.name}
+          </Typography>
+          <Typography component="p">{product.description}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Reviews
+        </Button>
+        <Button size="small" color="primary">
+          Add To Cart
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+MakeCard.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(MakeCard)
