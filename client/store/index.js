a353fd95c8a048bee28d8e35c03e4c5b74c7339f
Daniel Casasola <casasolad86@gmail.com>
2018-11-01 11:23:03 -0500

    created store code for list of items based on category

--- client/store/index.js
+++ client/store/index.js
@@ -4,10 +4,12 @@ import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import user from './user'
 import products from './product'
+import category from './category'
 
 const reducer = combineReducers({
   user,
-  products
+  products,
+  category
 })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
@@ -17,3 +19,4 @@ const store = createStore(reducer, middleware)
 export default store
 export * from './user'
 export * from './product'
+export * from './category'
