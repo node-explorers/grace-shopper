c5d26ee1824d3b9746ffea82799757bb2c993ad4
Luke <lukemiller58@yahoo.com>
2018-11-01 11:01:59 -0500

    Cart add and view completed

--- client/app.js
+++ client/app.js
@@ -3,11 +3,14 @@ import React from 'react'
 import { Navbar } from './components'
 import Routes from './routes'
 
+import Cart from './components/Cart'
+
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
+      <Cart />
     </div>
   )
 }
--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,14 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  Button
-} from 'reactstrap'
+
 import { fetchCartThunk } from '../store/cart'
 
 class Cart extends Component {
@@ -35,16 +27,16 @@ class Cart extends Component {
       <Fragment>
         <h2>Your Cart!</h2>
         <div>
-          {!this.props.cart.length ? (
+          {!this.props.cart.cartItems ? (
             <p>No Items</p>
           ) : (
             <div>
-              {this.props.cart.map(item => {
+              {this.props.cart.cartItems.map(item => {
                 return (
                   <div key={item.id}>
-                    <h1>{item.name}</h1>
+                    <h1>{item.product.name}</h1>
                     <small>{item.price}</small>
-                    <img src={item.imageUrl} />
+                    <img src={item.product.imageUrl} />
                     <h4>Quantity: {item.quantity}</h4>
                     <span>
                       {/* change quantity */}
