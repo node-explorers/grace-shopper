b4582027754f324acf13ae0f0444b97bf504f357
Daniel Casasola <casasolad86@gmail.com>
2018-11-02 12:00:30 -0500

    added checkoutForm

---
+++ client/components/CheckoutForm.js
@@ -0,0 +1,22 @@
+import  React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles'
+import MenuItem from '@material-ui/core/MenuItem'
+import TextField from '@material-ui/core/TextField'
+import { updatingProduct } from '../../store/product'
+import Button from '@material-ui/core/Button'
+
+
+class CheckoutForm extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default CheckoutForm;
