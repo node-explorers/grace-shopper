50f63fb4c046c302dfe7de535859e4cf619aa6b6
Mark Bassett <bassmarkie@gmail.com>
2018-11-07 09:22:32 -0600

    stripe production

--- client/components/Checkout/CheckoutForm.js
+++ client/components/Checkout/CheckoutForm.js
@@ -4,12 +4,12 @@ import StripeCheckout from 'react-stripe-checkout'
 
 const STRIPE_PUBLISHABLE =
   process.env.NODE_ENV === 'production'
-    ? 'pk_live_MY_PUBLISHABLE_KEY'
+    ? 'pk_test_OPpoAfjmyeTltfqxtGlndpfh'
     : 'pk_test_OPpoAfjmyeTltfqxtGlndpfh'
 
 const PAYMENT_SERVER_URL =
   process.env.NODE_ENV === 'production'
-    ? 'http://myapidomain.com'
+    ? 'https://node-explorers.herokuapp.com'
     : '/api/checkout'
 
 const CURRENCY = 'USD'
